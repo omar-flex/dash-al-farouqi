41b5a7588e3866d1ba50a31584b7d3a2e7b05f42
omar-flex <omar.marie@flex-ops.com>
2025-02-16 16:11:34 +0300

    Users

--- app/Http/Requests/UserManagement/EditUserRequest.php
+++ app/Http/Requests/UserManagement/EditUserRequest.php
@@ -8,15 +8,13 @@ use Illuminate\Foundation\Http\FormRequest;
 class EditUserRequest extends FormRequest
 {
     use DefaultRequest;
-
-
     public function rules(): array
     {
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255|unique:users,email,' . $this->id,
             'role_id' => 'required|string|exists:roles,id',
-            'password' => 'sometimes|nullable|min:6',
+            'password' => 'sometimes|nullable|min:5',
         ];
     }
 }
