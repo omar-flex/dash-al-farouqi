fa1a4ee75e83cc6469d3ba21b6f9c18ac963824e
omar-flex <omar.marie@flex-ops.com>
2025-02-16 13:24:40 +0300

    change

--- resources/views/pages/apps/operation-management/outbounds/create.blade.php
+++ resources/views/pages/apps/operation-management/outbounds/create.blade.php
@@ -117,9 +117,16 @@
                                        disabled>
                             </div>
                         </div>
-                    @endisset
 
-                    @isset($outbound)
+                        <div class="col-md-3 mb-7">
+                            <label class="fw-semibold fs-6 mb-2"> Warehouse </label>
+                            <div class="input-group mb-5">
+                                <input type="text" class="form-control form-control-solid-bg"
+                                       value="{{ $outbound->EnterRequest?->Warehouse?->code }}"
+                                       disabled>
+                            </div>
+                        </div>
+
                         <div class="col-md-2 mb-7">
                             <label class="required fw-semibold fs-6 mb-2">Bound CPM per BL</label>
                             <input type="number" step="any" name="cpm" min="0"
@@ -150,11 +157,13 @@
                     <div class="col-md-6 mb-7">
                         <label class="required fw-semibold fs-6 mb-2"> General description Goods</label>
                         <textarea class="form-control form-control-solid-bg mb-2"
-                                  name="general_description_goods" style="min-height: 30px" placeholder="General description Goods">@isset($outbound){{ $outbound->general_description_goods }}@endisset</textarea>
+                                  name="general_description_goods" style="min-height: 30px"
+                                  placeholder="General description Goods">@isset($outbound){{ $outbound->general_description_goods }}@endisset</textarea>
                     </div>
                     <div class="col-md-6 mb-7">
                         <label class="fw-semibold fs-6 mb-2"> Notes</label>
-                        <textarea class="form-control form-control-solid-bg mb-2" name="notes" style="min-height: 30px" placeholder="Notes">@isset($outbound){{ $outbound->notes }}@endisset</textarea>
+                        <textarea class="form-control form-control-solid-bg mb-2" name="notes" style="min-height: 30px"
+                                  placeholder="Notes">@isset($outbound){{ $outbound->notes }}@endisset</textarea>
                     </div>
 
                     <div class="col-md-8 mb-7">
