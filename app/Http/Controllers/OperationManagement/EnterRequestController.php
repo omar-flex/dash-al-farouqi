fd95c536ee60b9618efb801e8a80573aa8e8fbd1
omar-flex <omar.marie@flex-ops.com>
2025-04-21 11:57:39 +0300

    Remove debug statement from file upload process
    The `dd` debug statement was removed from the EnterRequestController. This ensures the file upload process proceeds without interruptions and avoids exposing sensitive data during runtime.

--- app/Http/Controllers/OperationManagement/EnterRequestController.php
+++ app/Http/Controllers/OperationManagement/EnterRequestController.php
@@ -324,7 +324,6 @@ use Illuminate\Support\Str;
                 $extension = $file->getClientOriginalExtension();
                 $cleanName = preg_replace('/[^A-Za-z0-9\.\-_]/', '-', $file->getClientOriginalName());
                 $path = Storage::putFileAs('Inbounds', $file, $cleanName);
-                dd($path,$file);
                 EnterRequestFile::create([
                     'filename' => Str::replace('/', '-', $enterRequest->bound_number),
                     'path' => $path,
