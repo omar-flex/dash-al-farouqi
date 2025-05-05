e2692a013aa515aee40e2751846809ecc2beff64
omar-flex <omar.marie@flex-ops.com>
2025-05-05 13:52:30 +0300

    Add debug statement to inspect file cleanName output
    A temporary debug statement (dd) was added to check the cleaned filename during file processing. This will help in identifying issues with filename sanitization and ensure correct handling before moving forward.

--- app/Http/Controllers/OperationManagement/EnterRequestController.php
+++ app/Http/Controllers/OperationManagement/EnterRequestController.php
@@ -319,6 +319,7 @@ use phpseclib3\File\ASN1\Maps\UniqueIdentifier;
             foreach (request('files') as $file) {
                 $extension = $file->getClientOriginalExtension();
                 $cleanName = preg_replace('/[^A-Za-z0-9\.\-_]/', '-', $file->getClientOriginalName());
+                dd($cleanName);
                 $uniqueName = $cleanName . '-' . uniqid() . '.' . $extension;
                 $path = Storage::disk('s3')->putFileAs('Inbounds', $file, $uniqueName);
                 EnterRequestFile::create([
