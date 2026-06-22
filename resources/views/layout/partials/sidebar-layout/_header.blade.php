dbcf2624e28b4f5ee3904de9897e75583237543c
omar-flex <omar.marie@flex-ops.com>
2026-06-22 13:10:12 +0300

    Update logo image paths to new version across multiple templates

--- resources/views/layout/partials/sidebar-layout/_header.blade.php
+++ resources/views/layout/partials/sidebar-layout/_header.blade.php
@@ -10,7 +10,7 @@
 		<!--begin::Mobile logo-->
 		<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
 			<a href="{{ route('dashboard') }}" class="d-lg-none">
-				<img alt="Logo" src="{{ image('logos/default-dark-new.png') }}" class="h-30px" />
+				<img alt="Logo" src="{{ image('logos/default-dark.png') }}" class="h-30px" />
 			</a>
 		</div>
 		<!--end::Mobile logo-->
